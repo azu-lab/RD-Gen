adbb9024d0b94588a1f98d0f766a16719c412c49
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-02-13 08:48:53 +0900

    Add function load_normal_config

--- src/normal_generate.py
+++ src/normal_generate.py
@@ -19,8 +19,35 @@ def option_parser() -> Union[argparse.FileType, str]:
     return args.config_yaml_path, args.dest_dir
 
 
-def main(config_yaml_file, dest_dir):
+def load_normal_config(config_yaml_file):
     config = file_handling_helper.load_config(config_yaml_file)
+    
+    compulsory_params = ['Number of DAGs',
+                         'Initial seed',
+                         'Initial node index',
+                         'Number of nodes',
+                         'Number of entry nodes',
+                         'Number of exit nodes',
+                         'In-degree',
+                         'Out-degree',
+                         'Execution time']
+    
+    # check compulsory parameter exists
+    try:
+        for compulsory_param in compulsory_params:
+            config['Compulsory'][compulsory_param]
+    except KeyError:
+        print(f"Compulsory parameter '{compulsory_param}' is not specified.")
+        exit(1)
+    
+    # TODO: 入力された型のエラーハンドリング
+    
+    return config
+
+
+def main(config_yaml_file, dest_dir):
+    config = load_normal_config(config_yaml_file)
+    print(config)
     print('TODO: 処理を書く')
 
 
