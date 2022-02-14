03096265b4c0aa95e586cdc2f56c2e1e105f1326
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-02-15 06:09:24 +0900

    Add validation check of In-degree and Out-degree

--- src/file_handling_helper.py
+++ src/file_handling_helper.py
@@ -24,7 +24,6 @@ def error_show_normal_config_format() -> None:
     print('Initial node index: <int>')
     print('Number of nodes: <int>')
     print('Number of entry nodes: <int>')
-    print('Number of exit nodes: <int>')
     print('In-degree:')
     print('  Min: <int>')
     print('  Max: <int>')
@@ -37,6 +36,8 @@ def error_show_normal_config_format() -> None:
     print('  Use list: [<int>, ..., <int>]  # (optional)')
     print('\n')
     print('# Optional parameters')
+    print('Force merge to exit nodes:')
+    print('  Number of exit nodes: <int>')
     print('Use communication time:')
     print('  Min: <int>')
     print('  Max: <int>')
@@ -67,7 +68,6 @@ def load_normal_config(config_yaml_file) -> Dict:
                          'Initial node index',
                          'Number of nodes',
                          'Number of entry nodes',
-                         'Number of exit nodes',
                          'In-degree',
                          'Out-degree',
                          'Execution time']
@@ -77,7 +77,7 @@ def load_normal_config(config_yaml_file) -> Dict:
         for compulsory_param in compulsory_params:
             config[compulsory_param]
     except KeyError:
-        print(f"Compulsory parameter '{compulsory_param}' is not specified.")
+        print(f"[Error] Compulsory parameter '{compulsory_param}' is not specified.")
         error_show_normal_config_format()
     
     # check format
@@ -85,50 +85,59 @@ def load_normal_config(config_yaml_file) -> Dict:
         if(input_param in ['In-degree', 'Out-degree']):  # 'Min' and 'Max'
             if(isinstance(config[input_param]['Min'], int) == False or
                     isinstance(config[input_param]['Max'], int) == False):
-                print("Type of 'Min' and 'Max' must be <int>.")
+                print("[Error] Type of 'Min' and 'Max' must be <int>.")
                 error_show_normal_config_format()
                 
         elif(input_param in ['Execution time', 'Use communication time']):  # 'Min', 'Max', and 'Use list'
             if('Use list' in config[input_param].keys()):
                 if(check_list_int(config[input_param]['Use list']) == False):
-                    print("Type of 'Use list' values must be <int>.")
+                    print("[Error] Type of 'Use list' values must be <int>.")
                     error_show_normal_config_format()
             else:
                 if(isinstance(config[input_param]['Min'], int) == False or
                         isinstance(config[input_param]['Max'], int) == False):
-                    print("Type of 'Min' and 'Max' must be <int>.")
+                    print("[Error] Type of 'Min' and 'Max' must be <int>.")
                     error_show_normal_config_format()
+        
+        elif(input_param == 'Force merge to exit nodes'):
+            try:
+                if(isinstance(config[input_param]['Number of exit nodes'], int) == False):
+                    print("[Error] Type of 'Number of exit nodes' must be <int>.")
+                    error_show_normal_config_format()
+            except KeyError:
+                print(f"'[Error] Number of exit nodes' is not specified.")
+                error_show_normal_config_format()
         
         elif(input_param == 'Use multi-period'):
             try:
                 periodic_type = config[input_param]['Periodic type']
                 if(periodic_type not in ['All', 'Entry', 'Random']):
-                    print(f"'Periodic type' must be 'All', 'Entry', or 'Random'.")
+                    print(f"'[Error] Periodic type' must be 'All', 'Entry', or 'Random'.")
                     error_show_normal_config_format()
             except KeyError:
-                print(f"'Periodic type' is not specified.")
+                print(f"'[Error] Periodic type' is not specified.")
                 error_show_normal_config_format()
             
             try:
                 if(isinstance(config[input_param]['Descendants have smaller period'], bool) == False):
-                    print("Type of 'Descendants have smaller period' must be <bool>.")
+                    print("[Error] Type of 'Descendants have smaller period' must be <bool>.")
                     error_show_normal_config_format()
             except KeyError:
-                print(f"'Descendants have smaller period' is not specified.")
+                print(f"'[Error] Descendants have smaller period' is not specified.")
                 error_show_normal_config_format()
             
             if('Use list' in config[input_param].keys()):
                 if(check_list_int(config[input_param]['Use list']) == False):
-                    print("Type of 'Use list' values must be <int>.")
+                    print("[Error] Type of 'Use list' values must be <int>.")
                     error_show_normal_config_format()
             else:
                 if(isinstance(config[input_param]['Min'], int) == False or
                         isinstance(config[input_param]['Max'], int) == False):
-                    print("Type of 'Min' and 'Max' must be <int>.")
+                    print("[Error] Type of 'Min' and 'Max' must be <int>.")
                     error_show_normal_config_format()
         else:
             if(isinstance(config[input_param], int) == False):
-                print(f"Type of '{input_param}' must be <int>.")
+                print(f"[Error] Type of '{input_param}' must be <int>.")
                 error_show_normal_config_format()
 
     return config
--- src/normal_generate.py
+++ src/normal_generate.py
@@ -1,6 +1,9 @@
 import argparse
 import file_handling_helper
-from typing import Union
+from typing import Union, Dict
+import networkx as nx
+import random
+from matplotlib import pyplot as plt
 
 
 def option_parser() -> Union[argparse.FileType, str]:
@@ -19,9 +22,48 @@ def option_parser() -> Union[argparse.FileType, str]:
     return args.config_yaml_path, args.dest_dir
 
 
-def main(config, dest_dir):
-    print('TODO: 処理を書く')
+def random_get_exec_time(config : Dict) -> int:
+    if('Use list' in config['Execution time'].keys()):
+        return random.choice(config['Execution time']['Use list'])
+    else:
+        return random.randint(config['Execution time']['Min'], config['Execution time']['Max'])
 
+
+def main(config, dest_dir):
+    # Validation check of In-degree and Out-degree
+    if(config['In-degree']['Max'] < config['Out-degree']['Min']):
+        print("[Error] Please increase 'Max' of 'In-degree' or decrease 'Min' of 'Out-degree'.")
+        exit(1)
+    if(config['Out-degree']['Max'] < config['In-degree']['Min']):
+        print("[Error] Please increase 'Max' of 'Out-degree' or decrease 'Min' of 'In-degree'.")
+        exit(1)
+    
+    # Generarte DAGs
+    for dag_i in range(config['Number of DAGs']):
+        # Initialize variables
+        random.seed(config['Initial seed'] + dag_i)
+        node_i = config['Initial node index']
+        G = nx.DiGraph()
+        
+        # Add entry nodes
+        for i in range(config['Number of entry nodes']):
+            G.add_node(node_i, execution_time=random_get_exec_time(config))
+            node_i += 1
+        
+        nx.draw(G, pos=nx.planar_layout(G))
+        plt.savefig('test.pdf')
+        
+        # TODO: 後続ノードがないノードに対して、子を生成する処理を繰り返す
+        # while(G.number_of_nodes() == config['Number of nodes']):
+        #     leaves = [v for v, d in G.out_degree() if d == 0]
+        #     for leaf in leaves:
+                
+            
+        
+        # TODO: 強制マージなら exit node を追加し、後続ノードがないノードをすべて繋ぐ
+        # TODO: 通信時間を使うなら、通信時間をランダムに決める
+        # TODO: 最後に Periodic type に応じて、周期タスクにする
+        # TODO: 描画（別関数）
 
 if __name__ == '__main__':
     config_yaml_file, dest_dir = option_parser()
