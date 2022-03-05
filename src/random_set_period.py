5754b8a7549f874e6b8ed6a745edd865fa51118e
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-03-05 20:39:39 +0900

    Add: params 'Entry node periods' and 'Exit node periods'

--- src/random_set_period.py
+++ src/random_set_period.py
@@ -1,20 +1,21 @@
 import networkx as nx
 import numpy as np
 import random
+import copy
 
 
-def _random_get_period(node_i, config, dag: nx.DiGraph) -> int:
+def _random_get_period(node_i, conf, dag: nx.DiGraph) -> int:
     lower_bound = None
 
     # Determine lower_bound
-    if(config['Use multi-period']['Periodic type'] == 'Chain'):
+    if(conf['Use multi-period']['Periodic type'] == 'Chain'):
         # TODO
         pass
     else:
         lower_bound = np.ceil(dag.nodes[node_i]['execution_time']
-                              / config['Use multi-period']['Max ratio of execution time to period'])
+                              / conf['Use multi-period']['Max ratio of execution time to period'])
 
-    if(config['Use multi-period']['Descendants have larger period']):
+    if(conf['Use multi-period']['Descendants have larger period']):
         ancestors = list(nx.ancestors(dag, node_i))
         anc_periods = []
         for ancestor in ancestors:
@@ -24,40 +25,69 @@ def _random_get_period(node_i, config, dag: nx.DiGraph) -> int:
             lower_bound = max(max(anc_periods), lower_bound)
 
     # Choice period
-    if('Use list' in config['Use multi-period'].keys()):
-        period_options = [t for t in config['Use multi-period']['Use list']
+    if('Use list' in conf['Use multi-period'].keys()):
+        period_options = [t for t in conf['Use multi-period']['Use list']
                           if t >= lower_bound]
         return random.choice(period_options)
     else:
-        return random.randint(lower_bound, config['Use multi-period']['Max'])
+        return random.randint(lower_bound, conf['Use multi-period']['Max'])
 
 
-def _set_all(config, G):
+def _set_all(conf, G: nx.DiGraph) -> None:
     for node_i in G.nodes():
-        G.nodes[node_i]['period'] = _random_get_period(node_i, config, G)
+        G.nodes[node_i]['period'] = _random_get_period(node_i, conf, G)
+    
+    if('Entry node periods' in conf['Use multi-period'].keys()):
+        _set_entry(conf, G)
+    if('Exit node periods' in conf['Use multi-period'].keys()):
+        _set_exit(conf, G)
 
 
-def _set_entry(config, G):
+def _set_entry(conf, G: nx.DiGraph) -> None:
     entry_nodes = [v for v, d in G.in_degree() if d == 0]
-    for entry_i in entry_nodes:
-        G.nodes[entry_i]['period'] = _random_get_period(entry_i, config, G)
+    if('Entry node periods' in conf['Use multi-period'].keys()):
+        unused_entry_periods = copy.deepcopy(conf['Use multi-period']['Entry node periods'])
+        for entry_i in entry_nodes:
+            if(not unused_entry_periods):
+                unused_entry_periods = copy.deepcopy(conf['Use multi-period']['Entry node periods'])
+            choose_period = random.choice(unused_entry_periods)
+            G.nodes[entry_i]['period'] = choose_period
+            unused_entry_periods.remove(choose_period)
+    else:
+        for entry_i in entry_nodes:
+            G.nodes[entry_i]['period'] = _random_get_period(entry_i, conf, G)
 
 
-def _set_chain(config, G):
+def _set_exit(conf, G: nx.DiGraph) -> None:
+    exit_nodes = [v for v, d in G.out_degree() if d == 0]
+    if('Exit node periods' in conf['Use multi-period'].keys()):
+        unused_exit_periods = copy.deepcopy(conf['Use multi-period']['Exit node periods'])
+        for exit_i in exit_nodes:
+            if(not unused_exit_periods):
+                unused_exit_periods = copy.deepcopy(conf['Use multi-period']['Exit node periods'])
+            choose_period = random.choice(unused_exit_periods)
+            G.nodes[exit_i]['period'] = choose_period
+            unused_exit_periods.remove(choose_period)
+    else:
+        for exit_i in exit_nodes:
+            G.nodes[exit_i]['period'] = _random_get_period(exit_i, conf, G)
+
+
+def _set_chain(conf, G):
     # TODO
     pass
 
 
-def random_set_period(config, mode: str, G: nx.DiGraph) -> None:
+def random_set_period(conf, mode: str, G: nx.DiGraph) -> None:
     if(mode not in ['normal', 'chain']):
         print('[Error] Invalid mode name was specified.')
         exit(1)
 
-    if(config['Use multi-period']['Periodic type'] == 'All'):
-        _set_all(config, G)
-    elif(config['Use multi-period']['Periodic type'] == 'Entry'):
-        _set_entry(config, G)
+    if(conf['Use multi-period']['Periodic type'] == 'All'):
+        _set_all(conf, G)
+    elif(conf['Use multi-period']['Periodic type'] == 'Entry'):
+        _set_entry(conf, G)
 
     if(mode == 'chain' and
-            config['Use multi-period']['Periodic type'] == 'Chain'):
-        _set_chain(config, G)
+            conf['Use multi-period']['Periodic type'] == 'Chain'):
+        _set_chain(conf, G)
