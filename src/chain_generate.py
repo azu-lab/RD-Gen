34c709e3fe2c1b2be74feb0b96f196b1a48e4f38
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-03-06 13:02:25 +0900

    Add: function _generate_chain

--- src/chain_generate.py
+++ src/chain_generate.py
@@ -2,11 +2,8 @@ import numpy as np
 import networkx as nx
 import random
 import copy
-import yaml
-import json
 import itertools
 from typing import List, Tuple
-from networkx.readwrite import json_graph
 
 from utils import option_parser, set_end_to_end_deadlines, random_get_comm_time, random_get_exec_time
 from file_handling_helper import load_chain_config
@@ -15,7 +12,44 @@ from random_set_period import random_set_period
 
 
 def _generate_chain(conf, num_nodes: int, G: nx.DiGraph) -> List[int]:
-    pass # TODO
+    # Determine the number of nodes in each sub chain  # HACK
+    first_sub_len = random.randint(conf['Chain length']['Min'], conf['Chain length']['Max'])
+    sub_len_options = list(range(1, conf['Chain length']['Max']+1))
+    sub_len_combos = []
+    while(not sub_len_combos):
+        first_sub_len = random.randint(conf['Chain length']['Min'], conf['Chain length']['Max'])
+        remain_num_nodes = num_nodes - first_sub_len
+        chain_width = random.randint(conf['Chain width']['Min']-1, conf['Chain width']['Max']-1)
+        for sub_len_combo in itertools.combinations_with_replacement(sub_len_options, chain_width):
+            if(sum(sub_len_combo) == remain_num_nodes and
+                    len([i for i, v in enumerate(sub_len_combo) if v == conf['Chain length']['Max']]) <= 1):
+                sub_len_combos.append(sub_len_combo)
+    sub_chain_len_combo = list(random.choice(sub_len_combos)) + [first_sub_len]
+
+    # Generate sub chains
+    sub_chain_dict = {str(sub_chain_i):[] for sub_chain_i in range(len(sub_chain_len_combo))}
+    for sub_chain_i, sub_chain_len in enumerate(sub_chain_len_combo):
+        for _ in range(sub_chain_len):
+            sub_chain_dict[str(sub_chain_i)].append(G.number_of_nodes())
+            G.add_node(G.number_of_nodes(), execution_time=random_get_exec_time(conf))
+            if(len(sub_chain_dict[str(sub_chain_i)]) >= 2):
+                G.add_edge(sub_chain_dict[str(sub_chain_i)][-2], sub_chain_dict[str(sub_chain_i)][-1])
+    
+    # Merge into the longest subchain
+    longest_subchain_i = sub_chain_len_combo.index(max(sub_chain_len_combo))
+    longest_sub_chain = sub_chain_dict[str(longest_subchain_i)]
+    chain = copy.deepcopy(sub_chain_dict[str(longest_subchain_i)])
+    for sub_chain_i, nodes in sub_chain_dict.items():
+        if(sub_chain_i == str(longest_subchain_i)):
+            continue
+        if(conf['Chain length']['Max'] - len(nodes) == 1):
+            source_i = longest_sub_chain[0]
+        else:
+            source_i = random.choice(longest_sub_chain[0:(conf['Chain length']['Max']-len(nodes)+1)])
+        G.add_edge(source_i, nodes[0])
+        chain += nodes
+
+    return chain
 
 
 def main(conf, dest_dir):
@@ -27,20 +61,27 @@ def main(conf, dest_dir):
         min_nodes = conf['Chain length']['Min'] + conf['Chain width']['Min'] - 1
         max_nodes = conf['Chain length']['Max']*conf['Chain width']['Max'] \
                             - conf['Chain width']['Max'] + 1
-        combination_options = []
-        for combination in itertools.combinations(
+        combos = []
+        for combo in itertools.combinations_with_replacement(
                 list(range(min_nodes, max_nodes+1)), conf['Number of chains']):
-            if(sum(combination) == conf['Number of nodes']):
-                combination_options.append(list(combination))
-        combination = random.choice(combination_options)
-        random.shuffle(combination)
+            if(sum(combo) == conf['Number of nodes']):
+                combos.append(list(combo))
+        chain_num_nodes_combo = random.choice(combos)
+        random.shuffle(chain_num_nodes_combo)
 
         # Generate each chain
         chains = []
-        for num_nodes_in_one_chain in combination:
-            chains.append(_generate_chain(conf, num_nodes_in_one_chain))
+        for num_nodes_in_one_chain in chain_num_nodes_combo:
+            chains.append(_generate_chain(conf, num_nodes_in_one_chain, G))
         
         # TODO: gen dag
+
+        # (Optional) Use multi-period
+        if('Use multi-period' in conf.keys()):
+            random_set_period(conf, G)
+
+        # Set execution time
+        
 
         # (Optional) Use communication time
         if('Use communication time' in conf.keys()):
@@ -51,28 +92,6 @@ def main(conf, dest_dir):
         # (Optional) Use end-to-end deadline
         if('Use end-to-end deadline' in conf.keys()):
             set_end_to_end_deadlines(conf, G)
-
-        # (Optional) Use multi-period
-        if('Use multi-period' in conf.keys()):
-            random_set_period(conf, G)
-
-        # Output
-        dag_formats = [k for k, v in conf['DAG format'].items() if v]
-        if('xml' in dag_formats):
-            nx.write_graphml_xml(G, f'{dest_dir}/dag_{dag_i}.xml')
-        if('dot' in dag_formats):
-            nx.drawing.nx_pydot.write_dot(G, f'{dest_dir}/dag_{dag_i}.dot')
-        if('json' in dag_formats):
-            data = json_graph.node_link_data(G)
-            s = json.dumps(data)
-            with open(f'{dest_dir}/dag_{dag_i}.json', 'w') as f:
-                json.dump(s, f)
-        if('yaml' in dag_formats):
-            data = json_graph.node_link_data(G)
-            s = json.dumps(data)
-            dic = json.loads(s)
-            with open(f'{dest_dir}/dag_{dag_i}.yaml', 'w') as f:
-                yaml.dump(dic, f)
 
         write_dag(conf, dest_dir, f'dag_{dag_i}', G)
 
