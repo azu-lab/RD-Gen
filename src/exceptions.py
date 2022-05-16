a9b9cdce84454d8f35526582c371c7ffba0cd1d0
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-05-17 05:00:54 +0900

    fix: remove unused files

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,16 +2,6 @@ class Error(Exception):
     """Base class for exception in this module."""
 
 
-class NoSettablePeriodError(Error):
-    def __init__(self, message: str = '') -> None:
-        self.message = message
-
-
-class InvalidConfigError(Error):
-    def __init__(self, message: str = '') -> None:
-        self.message = message
-
-
 class MaxBuildFailError(Error):
     def __init__(self, message: str = '') -> None:
         self.message = message
--- test.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-
-G = nx.DiGraph()
-G.add_nodes_from(list(range(5)))
-
-nx.add_star(G, [0, 6, 7, 8])
-
-print(G.nodes)
-print(G.succ[0])
